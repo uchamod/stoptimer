f8cb778c9077f3685077f6b5cf780ce1d9826549
Chamod Udara <128582580+uchamod@users.noreply.github.com>
2023-12-12 23:39:35 +0530

    Initial commit

---
+++ src/stoptimer/Stoptimer.java
@@ -0,0 +1,14 @@
+
+package stoptimer;
+
+
+public class Stoptimer {
+
+    
+    public static void main(String[] args) {
+        stopwatch stop =new stopwatch();
+        stop.setVisible(true);
+        stop.setLocationRelativeTo(null);
+    }
+    
+}
